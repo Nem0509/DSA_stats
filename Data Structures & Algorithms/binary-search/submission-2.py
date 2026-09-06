41ad218c63bc40e6d7cff931329f928e5f4e4ecf
Shashank Dhakar <shashanksantoshdhakar19@gmail.com>
2026-09-06 17:18:06 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.py
@@ -0,0 +1,10 @@
+import bisect
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        ans=bisect.bisect_left(nums,target)
+        if ans<len(nums) and nums[ans]==target:
+            return ans
+        else:
+            return -1
+        
